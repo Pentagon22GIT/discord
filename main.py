d963dccde2fd9e882ac12431a018051ee7869ec0
はっせー <153830524+Pentagon22GIT@users.noreply.github.com>
2024-06-26 12:01:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
 import discord
 import os
 from keep_alive import keep_alive
-from supabase import create_client, Client
-
-# SupabaseのURLとAPIキー
-supabase_url = os.getenv("SUPABASE_URL")
-supabase_key = os.getenv("SUPABASE_KEY")
-supabase: Client = create_client(supabase_url, supabase_key)
+from supabase import create_client
 
 client = discord.Client(intents=discord.Intents.default())
-
-
-def get_data():
-    try:
-        response = supabase.table("your_table_name").select("*").execute()
-        print(response.data)  # 取得したデータをログに出力
-        return response.data
-    except Exception as e:
-        print(f"Error fetching data: {e}")
-        return []
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_KEY")
+supabase = create_client(supabase_url, supabase_key)
 
 
 @client.event
@@ -29,20 +17,29 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith("!data"):
-        print(
-            "!dataコマンドがトリガーされました"
-        )  # コマンドがトリガーされたことをログに出力
-        data = get_data()
+        print("!dataコマンドがトリガーされました")
+        data = get_data_from_supabase()
         if not data:
             await message.channel.send("データを取得できませんでした。")
         else:
             response = ""
             for row in data:
-                response += f"日付け: {row['date']}, 名前: {row['name']}, 人数: {row['number_of_people']}\n"
+                response += f"日付: {row['date']}, 名前: {row['name']}, 人数: {row['number_of_people']}\n"
             await message.channel.send(response)
+    else:
+        await message.channel.send("このコマンドはサポートされていません。")
+
+
+def get_data_from_supabase():
+    query = "SELECT * FROM test"
+    response = supabase.query(query)
+    if response.error:
+        print(f"Supabase エラー: {response.error}")
+        return None
+    else:
+        return response.data
 
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-# Web サーバの立ち上げ
 keep_alive()
 client.run(TOKEN)
