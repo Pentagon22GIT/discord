25a398281d8dbb4c9e82b780652ca257e657be07
はっせー <153830524+Pentagon22GIT@users.noreply.github.com>
2024-06-26 10:27:55 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import discord
 import os
 from keep_alive import keep_alive
+from supabase import create_client, Client
+
+# SupabaseのURLとAPIキー
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(supabase_url, supabase_key)
 
 client = discord.Client(intents=discord.Intents.default())
+
+
+def get_data():
+    response = supabase.table("your_table_name").select("*").execute()
+    return response.data
 
 
 @client.event
@@ -12,8 +23,12 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    emoji = "👍"
-    await message.add_reaction(emoji)
+    if message.content.startswith("!data"):
+        data = get_data()
+        response = ""
+        for row in data:
+            response += f"日付け: {row['date']}, 名前: {row['name']}, 人数: {row['number_of_people']}\n"
+        await message.channel.send(response)
 
 
 TOKEN = os.getenv("DISCORD_TOKEN")
