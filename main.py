669f77106938521b75ac1470c88073d97ac0b289
はっせー <153830524+Pentagon22GIT@users.noreply.github.com>
2024-06-26 12:22:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,29 @@
 import discord
 import os
 from keep_alive import keep_alive
-from supabase import create_client
 
-client = discord.Client(intents=discord.Intents.default())
-supabase_url = os.getenv("SUPABASE_URL")
-supabase_key = os.getenv("SUPABASE_KEY")
-supabase = create_client(supabase_url, supabase_key)
 
-@client.event
-async def on_ready():
-    print("ログインしました")
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f"ログインしました: {self.user}")
 
-@client.event
-async def on_message(message):
-    if message.author.bot:  # ボット自身のメッセージには反応しない
-        return
-    
-    if message.content.startswith("!data"):
-        print("!dataコマンドがトリガーされました")
-        data = get_data_from_supabase()
-        if not data:
-            await message.channel.send("データを取得できませんでした。")
-        else:
-            response = ""
-            for row in data:
-                response += f"日付: {row['date']}, 名前: {row['name']}, 人数: {row['number_of_people']}\n"
-            await message.channel.send(response)
-    else:
-        # !data 以外のコマンドには反応しないようにする
-        pass
+    async def on_message(self, message):
+        print(f"送信: {message.author}: {message.content}")
+        if message.author == self.user:
+            return
 
-def get_data_from_supabase():
-    query = "SELECT * FROM test"
-    response = supabase.query(query)
-    if response.error:
-        print(f"Supabase エラー: {response.error}")
-        return None
-    else:
-        return response.data
+        if message.content == "$Hello":
+            await message.channel.send("Hello!")
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(os.getenv("TOKEN"))
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+
 keep_alive()
+
 client.run(TOKEN)
