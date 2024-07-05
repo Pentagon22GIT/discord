3d926eb68b8e10184ad5d6b4b897cb19a7f43b2f
はっせー <153830524+Pentagon22GIT@users.noreply.github.com>
2024-07-05 22:09:19 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -108,9 +108,9 @@ async def insert(interaction: discord.Interaction, name: str, content: str):
         response = (
             supabase.table("chat").insert({"name": name, "content": content}).execute()
         )
-        if response.status_code != 201:
+        if response.error:
             await interaction.followup.send(
-                f"①データの挿入中にエラーが発生しました: {response.json()}"
+                f"データの挿入中にエラーが発生しました: {response.error.message}"
             )
         else:
             embed = discord.Embed(
@@ -125,7 +125,7 @@ async def insert(interaction: discord.Interaction, name: str, content: str):
             await interaction.followup.send(embed=embed)
     except Exception as e:
         await interaction.followup.send(
-            f"②データの挿入中にエラーが発生しました: {str(e)}"
+            f"データの挿入中にエラーが発生しました: {str(e)}"
         )
 
 
