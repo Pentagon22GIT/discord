7d4e80c27d5eafeb197ee14f647f16f3f1b42f3c
はっせー <153830524+Pentagon22GIT@users.noreply.github.com>
2024-07-05 21:54:32 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.presences = True
 
+
 class MyClient(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix="/", intents=intents)
@@ -20,12 +21,15 @@ class MyClient(commands.Bot):
     async def setup_hook(self):
         await self.tree.sync()
 
+
 client = MyClient()
+
 
 @client.event
 async def on_ready():
     await client.change_presence(activity=discord.Game(name="Supabaseに接続中"))
     print(f"ログインしました: {client.user}")
+
 
 @client.tree.command(name="setup", description="使い方の説明です")
 async def setup(interaction: discord.Interaction):
@@ -41,10 +45,16 @@ async def setup(interaction: discord.Interaction):
     embed.add_field(
         name="Test", value="テスト機能を使用することができます", inline=False
     )
+    embed.add_field(
+        name="Insert",
+        value="データベースを通してchatをすることができます",
+        inline=False,
+    )
     embed.set_footer(
         text="現在時刻での状況です",  # フッターには開発者の情報でも入れてみる
     )
     await interaction.response.send_message(embed=embed)
+
 
 @client.tree.command(name="get", description="Supabaseからデータを取得します")
 async def get(interaction: discord.Interaction):
@@ -79,6 +89,7 @@ async def get(interaction: discord.Interaction):
             f"データの取得中にエラーが発生しました: {str(e)}"
         )
 
+
 @client.tree.command(name="test", description="テスト機能を表示します")
 async def test(interaction: discord.Interaction):
     embed = discord.Embed(
@@ -88,6 +99,35 @@ async def test(interaction: discord.Interaction):
         url="https://example.com",  # これを設定すると、タイトルが指定URLへのリンクになる
     )
     await interaction.response.send_message(embed=embed)
+
+
+@client.tree.command(name="insert", description="Supabaseにデータを挿入します")
+async def insert(interaction: discord.Interaction, name: str, content: str):
+    await interaction.response.defer()
+    try:
+        response = (
+            supabase.table("chat").insert({"name": name, "content": content}).execute()
+        )
+        if response.error:
+            await interaction.followup.send(
+                f"データの挿入中にエラーが発生しました: {response.error.message}"
+            )
+        else:
+            embed = discord.Embed(
+                title="データ挿入完了",
+                color=0x00FF00,  # 緑色
+                description="Supabaseにデータが正常に挿入されました",
+            )
+            embed.add_field(name="Name", value=name, inline=True)
+            embed.add_field(name="Content", value=content, inline=True)
+            embed.set_footer(text="挿入されたデータの詳細")
+
+            await interaction.followup.send(embed=embed)
+    except Exception as e:
+        await interaction.followup.send(
+            f"データの挿入中にエラーが発生しました: {str(e)}"
+        )
+
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
